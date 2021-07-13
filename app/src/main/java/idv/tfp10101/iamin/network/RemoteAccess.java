f3078e5d4a5446e61ec4bbb711a9fdb16f52400d
王靖渝 <>
2021-07-13 22:22:01 +0800

    no message

--- app/src/main/java/idv/tfp10101/iamin/network/RemoteAccess.java
+++ app/src/main/java/idv/tfp10101/iamin/network/RemoteAccess.java
@@ -22,9 +22,9 @@ import idv.tfp10101.iamin.Constants;
  */
 public class RemoteAccess {
     // 根網址
-//    public static String URL_SERVER = "http://10.0.2.2:8080/iamin_JavaServlet/";
+    public static String URL_SERVER = "http://10.0.2.2:8080/iamin_JavaServlet/";
     //實機測試
-    public static String URL_SERVER = "http://192.168.1.101:8080/iamin_JavaServlet/";
+//    public static String URL_SERVER = "http://192.168.1.101:8080/iamin_JavaServlet/";
     //  test
     /**
      * (Json)抓取server資料
