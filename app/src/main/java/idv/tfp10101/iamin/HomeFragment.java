c2363eff7a6fc9ac58dcd94ae6a3b349740b4efc
林以樂 <a0989758574@gmail.com>
2021-08-11 15:25:45 +0800

    改用icon來跳轉至團購地圖 2021/08/11

--- app/src/main/java/idv/tfp10101/iamin/HomeFragment.java
+++ app/src/main/java/idv/tfp10101/iamin/HomeFragment.java
@@ -100,7 +100,7 @@ public class HomeFragment extends Fragment {
     private double userlat,userlng;//使用者的緯經度
     private List<HomeData> localHomeDatas;  //把團購跟使用者最短距離裝成Homedata
     private ProgressDialog Loading;
-    private TextView txv_map;
+    private ImageView imv_locate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -182,7 +182,7 @@ public class HomeFragment extends Fragment {
         Loading.setMessage("Loading...");
         Loading.setCancelable(false);
         Loading.show();
-        txv_map.setOnClickListener(v ->{
+        imv_locate.setOnClickListener(v ->{
             Navigation.findNavController(v).navigate(R.id.homeMapFragment);
         });
         //實作取得買家緯精度方法
@@ -444,7 +444,7 @@ public class HomeFragment extends Fragment {
         recyclerViewGroup.setLayoutManager(new LinearLayoutManager(activity));
         swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
         searchView = view.findViewById(R.id.searchview);
-        txv_map = view.findViewById(R.id.txv_map);
+        imv_locate = view.findViewById(R.id.imv_locate);
     }
 
     private class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyHomeDataViewHolder> {
